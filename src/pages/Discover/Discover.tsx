093ed8a08c91af6d81a938b3789b59b1b38288b9
Ben Naylor <nayben@gmail.com>
2020-03-01 20:08:09 +0100

    feat(discover): filter out current user leagues

--- src/pages/Discover/Discover.tsx
+++ src/pages/Discover/Discover.tsx
@@ -5,6 +5,7 @@ import { useHistory } from 'react-router-dom';
 // Utils
 import { League } from '../../interfaces/League';
 import { useLeagueService } from '../../services/leagues';
+import { useUser } from '../../stores/user';
 
 // Components
 import Title from '../../components/Title';
@@ -12,6 +13,7 @@ import { LeagueCard } from '../../components/league';
 
 const Discover: React.FC = () => {
   const history = useHistory();
+  const { user } = useUser();
   const { getDiscoverLeagues, join } = useLeagueService();
   const [leagues, setLeagues] = useState<Array<League>>([]);
 
@@ -25,16 +27,18 @@ const Discover: React.FC = () => {
       <Title>Discover</Title>
       <section className="content">
         {
-          leagues.map(league => (
-            <LeagueCard league={league} key={league.id} onClick={() => history.push(`/league/${league.id}`)}>
-              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <Button variant="contained" color="primary" style={{ margin: 8 }} onClick={() => join(league.id!).then(() => getDiscoverLeagues().then(leagues => setLeagues(leagues)))}>Join</Button>
-                <Typography component="h6" variant="subtitle2">
-                  {league.userCount} Player{league.userCount! > 1 ? 's' : ''}
-                </Typography>
-              </div>
-            </LeagueCard>
-          ))
+          leagues
+            .filter(league => !user || (user && !user.leagues[league.id!]))
+            .map(league => (
+              <LeagueCard league={league} key={league.id} onClick={() => history.push(`/league/${league.id}`)}>
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                  <Button variant="contained" color="primary" style={{ margin: 8 }} onClick={() => join(league.id!).then(() => getDiscoverLeagues().then(leagues => setLeagues(leagues)))}>Join</Button>
+                  <Typography component="h6" variant="subtitle2">
+                    {league.userCount} Player{league.userCount! > 1 ? 's' : ''}
+                  </Typography>
+                </div>
+              </LeagueCard>
+            ))
         }
       </section>
     </>
